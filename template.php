f197038745c5d36c8a22e0631ff8b2c896525d19
Braulio Soncco <soncco@gmail.com>
2012-07-18 12:18:04 -0500

    OG Empty

--- template.php
+++ template.php
@@ -180,14 +180,17 @@ if(drupal_is_front_page()) {
   $og_attributes = array('og:url', 'og:title', 'og:image', 'og:description', 'fb:app_id');
   
   foreach ($og_attributes as $k => $og_attribute) {
-    $element = array(
-      '#tag' => 'meta',
-      '#weight' => ($k + 10),
-      '#attributes' => array(
-        'property' => $og_attribute,
-        'content' => theme_get_setting($og_attribute),
-      ),
-    );
-    drupal_add_html_head($element, "qelluchaska_meta_$og_attribute");
+    if (!empty(theme_get_setting($og_attribute))) {
+      $element = array(
+        '#tag' => 'meta',
+        '#weight' => ($k + 10),
+        '#attributes' => array(
+          'property' => $og_attribute,
+          'content' => theme_get_setting($og_attribute),
+        ),
+      );
+      drupal_add_html_head($element, "qelluchaska_meta_$og_attribute");
+    }
   }
 }
+ 
